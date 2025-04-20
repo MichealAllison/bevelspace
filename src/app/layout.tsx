8223e9b44ee81ce7a812435b2dd82fd4e9f4ca98
Micheal Allison <michealallison220@gmail.com>
2025-04-20 19:32:50 +0100

    Implement website SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,43 @@ import type { Metadata } from "next";
 import Header from "@/components/layout/header/header";
 import "./globals.css";
 import Footer from "@/components/layout/footer/footer";
+
 export const metadata: Metadata = {
   title: "Bevel Space",
   description: "Where Real Estate Meets Clarity.",
+  keywords: ["real estate", "property management", "real estate technology", "property listings"],
+  authors: [{ name: "Bevel Space" }],
+  openGraph: {
+    title: "Bevel Space - Where Real Estate Meets Clarity",
+    description: "Transform your real estate experience with Bevel Space's innovative platform.",
+    type: "website",
+    url: "https://bevelspace.vercel.app/",
+    siteName: "Bevel Space",
+    locale: "en_US",
+    images: [
+      {
+       url:'images/bevelspacelogo.png',
+        width: 1200,
+        height: 630,
+        alt: "Bevel Space - Where Real Estate Meets Clarity",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Bevel Space - Where Real Estate Meets Clarity",
+    description: "Transform your real estate experience with Bevel Space's innovative platform.",
+    images: [
+      {
+        url: "images/bevelspacelogo.png",
+        width: 1200,
+      },
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({
