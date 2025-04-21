67e61af78d8f7cfaac65e008f13fdf922be8dcff
Micheal Allison <michealallison220@gmail.com>
2025-04-21 18:31:07 +0100

    fixes

---
+++ src/app/property/[id].tsx
@@ -0,0 +1,7 @@
+import PropertyDetail from "@/features/property/components/propertyDetailPage/propertyDetail";
+
+export default function PropertyDetailPage() {
+    return (
+        <PropertyDetail />
+    )
+}
---
+++ src/features/property/components/propertyDetailPage/propertyDetail.tsx
@@ -0,0 +1,9 @@
+const PropertyDetail = () => {
+    return (
+        <div>
+            <h1>Property Detail Page</h1>
+        </div>
+    )
+}
+
+export default PropertyDetail;
