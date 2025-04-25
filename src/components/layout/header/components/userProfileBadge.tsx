6f795f23f26dede61f86ca304ec3a5ac58aa8cba
Micheal Allison <michealallison220@gmail.com>
2025-04-25 20:27:09 +0100

    Implement log out data fetching

--- src/components/layout/header/components/userProfileBadge.tsx
+++ src/components/layout/header/components/userProfileBadge.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Settings, LogOut, HelpCircle, User, ChevronDown, Heart, Home } from "lucide-react"
+import { Settings, LogOut, HelpCircle, User, Heart, Home } from "lucide-react"
 import {
   DropdownMenu,
   DropdownMenuContent,
